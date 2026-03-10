53c762e08e93ec3df5a70564d18a42be5401c60e
Poojitha0215 <poojitha0215@gmail.com>
2026-03-10 10:17:36 +0530

    updated

--- models/productModel.js
+++ models/productModel.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 const productSchema = mongoose.Schema({
   name: { type: String, required: true},
   desc: { type: String, required: true},
