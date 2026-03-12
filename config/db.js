923e04e09e400aef798729ff5ef7b820fc976384
Poojitha0215 <poojitha0215@gmail.com>
2026-03-12 15:12:07 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
+
 const dbConnect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB Connected");
   } catch (err) {
-    console.log(err);
+    console.log("❌ MongoDB Connection Error:", err);
   }
 };
+
 export default dbConnect;
